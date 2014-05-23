dd5dcde7446b24fe08a67bcd66e8e738289b104b
kazizaman <kzaman@thirdwave.it>
2014-05-23 17:08:40 -0400

    Added US Phone number validation.
    This will test only use phone number.

---
+++ src/Crux.Core.Tests/Extensions/TelcoExtensionTester.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Crux.Core.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Crux.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class TelcoExtensionTester
+    {
+        [Test]
+        public void should_validate_us_phone_number()
+        {
+            "5617774825".IsUSPhoneNumber().Should().Be(true);
+            "(281)388-0388".IsUSPhoneNumber().Should().Be(true);
+            "(281)388-0300".IsUSPhoneNumber().Should().Be(true);
+            "(979) 778-0978".IsUSPhoneNumber().Should().Be(true);
+            "(281)934-2479".IsUSPhoneNumber().Should().Be(true);
+            "(281)934-2447".IsUSPhoneNumber().Should().Be(true);
+            "(979)826-3273".IsUSPhoneNumber().Should().Be(true);
+            "(979)826-3255".IsUSPhoneNumber().Should().Be(true);
+            "1334714149".IsUSPhoneNumber().Should().Be(true);
+            "(281)356-2530".IsUSPhoneNumber().Should().Be(true);
+            "(281)356-5264".IsUSPhoneNumber().Should().Be(true);
+            "(936)825-2081".IsUSPhoneNumber().Should().Be(true);
+            "(832)595-9500".IsUSPhoneNumber().Should().Be(true);
+            "(832)595-9501".IsUSPhoneNumber().Should().Be(true);
+            "281-342-2452".IsUSPhoneNumber().Should().Be(true);
+            "1334431660".IsUSPhoneNumber().Should().Be(true);
+            "1334431660222".IsUSPhoneNumber().Should().Be(false);
+        }
+
+        [Test]
+        public void should_thorw_exception_for_invalid_US_number()
+        {
+            Assert.Throws<InvalidDataException>(() => "foobar".AssertValidUSPhoneNumber(), "foobar is not a valid US phone number");
+            Assert.Throws<InvalidDataException>(() => "561777482".AssertValidUSPhoneNumber(), "561777482 is not a valid US phone number");
+            Assert.Throws<InvalidDataException>(() => "13344316601".AssertValidUSPhoneNumber(), "13344316601 is not a valid US phone number");
+        }
+    }
+}
---
+++ src/Crux.Core/Extensions/TelcoExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Crux.Core.Extensions
+{
+   public static class TelcoExtension
+   {
+       private const string USPHONE_REGEX = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+       private static readonly Regex USPHONE_VALIDATOR = new Regex(USPHONE_REGEX, RegexOptions.Compiled);
+
+       public static bool IsUSPhoneNumber(this string phone)
+       {
+           return USPHONE_VALIDATOR.IsMatch(phone);
+       }
+
+       public static void AssertValidUSPhoneNumber(this string phone)
+       {
+           if (!phone.IsUSPhoneNumber()) {
+               throw new InvalidDataException("{0} is not a valid US phone number.".ToFormat(phone));
+           }
+       }
+
+   }
+}
