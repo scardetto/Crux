a478ab1c85cdc3f73e56047c3716bc91060010c5
lhernandez <leo@booleansolutions.com>
2014-05-30 15:42:07 -0400

    added lib to support unit test of web api endpoints/resources

---
+++ src/Crux.WebApi.Tests/Assertions/AssertionsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace Crux.WebApi.Tests.Assertions
+{
+    public static class AssertionsExtensions
+    {
+        public static HttpResponseMessageAssertions Should(this HttpResponseMessage response)
+        {
+            return new HttpResponseMessageAssertions(response);
+        }
+    }
+}
