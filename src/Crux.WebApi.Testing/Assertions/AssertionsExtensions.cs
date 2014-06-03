98b3e5488f2db57732c8de31b6cbdf50a34300bc
lhernandez <leo@booleansolutions.com>
2014-06-03 10:27:05 -0400

    Forcing FluentAssertion configuration to run

--- src/Crux.WebApi.Testing/Assertions/AssertionsExtensions.cs
+++ src/Crux.WebApi.Testing/Assertions/AssertionsExtensions.cs
@@ -1,9 +1,16 @@
 ﻿using System.Net.Http;
+using System.Runtime.CompilerServices;
+using FluentAssertions;
 
 namespace Crux.WebApi.Testing.Assertions
 {
     public static class AssertionsExtensions
     {
+        static AssertionsExtensions()
+        {
+            RuntimeHelpers.RunClassConstructor(typeof (AssertionExtensions).TypeHandle);
+        }
+
         public static HttpResponseMessageAssertions Should(this HttpResponseMessage response)
         {
             return new HttpResponseMessageAssertions(response);
