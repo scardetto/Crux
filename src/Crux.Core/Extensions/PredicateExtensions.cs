97037dc8baa491ba5f5df53838a6e544d61e9009
kazizaman <kzaman@thirdwave.it>
2014-06-09 16:38:44 -0400

    Added extension for Predicate expression
    This allows to build dynamic filter.

--- src/Crux.Core/Extensions/PredicateExtensions.cs
+++ src/Crux.Core/Extensions/PredicateExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Crux.Core.Extensions
 {
@@ -17,6 +19,25 @@ namespace Crux.Core.Extensions
         public static Func<T, bool> Or<T>(this Func<T, bool> predicate1, Func<T, bool> predicate2)
         {
             return v => predicate1(v) || predicate2(v);
+        }
+
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
+                                                            Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+                                                             Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
         }
     }
 }
