db699a6fb242b4991d0f939553f4c0b6e8ccf6a3
kazizaman <kzaman@thirdwave.it>
2014-06-12 15:26:22 -0400

    Added Email Tests

---
+++ src/Crux.Core.Tests/Extensions/EmailExtensionsTester.cs
@@ -0,0 +1,23 @@
+﻿using Crux.Core.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Crux.Core.Tests.Extensions
+{
+    [TestFixture]
+   public class EmailExtensionsTester
+    {
+        [Test]
+        public void should_validate_email_address()
+        {
+            "jhondoe@yahoo.com".IsEmailAddress().Should().BeTrue();
+            "jhon.doe@yahoo.com".IsEmailAddress().Should().BeTrue();
+            "jhondoe@yahoo.com".IsEmailAddress().Should().BeTrue();
+            "jhon.doe@yahoo.net".IsEmailAddress().Should().BeTrue();
+            "jhondoe@yahoo.org".IsEmailAddress().Should().BeTrue();
+            "jhondoe@yahoo.us".IsEmailAddress().Should().BeTrue();
+            "jhondoe@yahoo.it".IsEmailAddress().Should().BeTrue();
+
+        }
+    }
+}
