3445bfe0ee85a0c46958bec0b722a0a135ac6b47
lhernandez <leo@booleansolutions.com>
2014-12-11 12:41:07 -0500

    NancyFx functionality

---
+++ src/Crux.NancyFx/Infrastructure/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Nancy.Validation;
+
+namespace Crux.NancyFx.Infrastructure.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(ModelValidationResult modelValidationResult)
+        {
+            ModelModelValidationResult = modelValidationResult;
+        }
+
+        public ModelValidationResult ModelModelValidationResult { get; private set; }
+    }
+}
---
+++ src/Crux.NancyFx/Mappings/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using Crux.Core.Bootstrapping;
+
+namespace Crux.NancyFx.Mappings
+{
+    public abstract class MapperConfig : IRunAtStartup
+    {
+        public void Init()
+        {
+            ToModel();
+            ToDomain();
+        }
+
+        protected virtual void ToDomain() { }
+
+        protected virtual void ToModel() { }
+    }
+}
