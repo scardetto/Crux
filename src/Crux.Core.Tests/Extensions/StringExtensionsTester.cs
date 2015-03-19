fd5e1c732c3feea58ff3460ec4991ba30ce98617
Rob Scaduto <rob@thirdwave.it>
2015-03-19 13:59:57 -0400

    Convert to universal time before comparison

--- src/Crux.Core.Tests/Extensions/StringExtensionsTester.cs
+++ src/Crux.Core.Tests/Extensions/StringExtensionsTester.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Net.Sockets;
 using System.Text;
 using Crux.Core.Extensions;
 using FluentAssertions;
@@ -184,7 +185,8 @@ namespace Crux.Core.Tests.Extensions
         [Test]
         public void toNullableDateTime_should_return_datetime_for_iso8601_input()
         {
-            "2014-11-06T00:00:00-05:00".ToNullableDateTime().Should().Be(new DateTimeOffset(new DateTime(2014, 11, 6), new TimeSpan(-5, 0, 0)).DateTime);
+            var utcActual = "2014-11-06T00:00:00-05:00".ToNullableDateTime().Value.ToUniversalTime();
+            utcActual.Should().Be(new DateTimeOffset(new DateTime(2014, 11, 6), new TimeSpan(-5, 0, 0)).DateTime.ToUniversalTime());
         }
     }
 }
