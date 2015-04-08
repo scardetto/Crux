1e0fff2ed4b8a49b94ef14947b5fea3081268c54
Leo Hernandez <lhernandez@thirdwave.it>
2015-04-08 15:52:27 -0400

    Method extension

---
+++ src/Crux.NancyFx/Infrastructure/Extensions/NancyContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Crux.NancyFx.Infrastructure.Extensions
+{
+    public static class NancyContextExtensions
+    {
+        public static bool IsHttpOptions(this NancyContext context)
+        {
+            return context.Request.Method.Equals("OPTIONS");
+        }
+    }
+}
