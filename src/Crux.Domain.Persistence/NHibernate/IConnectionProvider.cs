27c99f82bc2d92871c43e01d0d2460d3a4783b05
Rob Scaduto <rob@thirdwave.it>
2015-04-17 01:18:26 -0400

    Added IConnectionProvider to UoW connstructor

---
+++ src/Crux.Domain.Persistence/NHibernate/IConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Crux.Domain.Persistence.NHibernate
+{
+    public interface IDbConnectionProvider
+    {
+        IDbConnection GetConnection();
+    }
+}
--- src/Crux.Domain.Persistence/NHibernate/NHibernateUnitOfWork.cs
+++ src/Crux.Domain.Persistence/NHibernate/NHibernateUnitOfWork.cs
@@ -12,6 +12,7 @@ namespace Crux.Domain.Persistence.NHibernate
     public class NHibernateUnitOfWork : INHibernateUnitOfWork
     {
         private readonly ISessionFactory _factory;
+        private readonly IDbConnectionProvider _connectionProvider;
 
         public ISession CurrentSession { get; private set; }
 
@@ -30,16 +31,17 @@ namespace Crux.Domain.Persistence.NHibernate
             return new TransactionalUnitOfWorkScope(this, new NHibernateUnitOfWorkTransaction(this, options));
         }
 
-        public NHibernateUnitOfWork(ISessionFactory factory)
+        public NHibernateUnitOfWork(ISessionFactory factory, IDbConnectionProvider connectionProvider)
         {
             _factory = factory;
+            _connectionProvider = connectionProvider;
         }
 
         public void Start()
         {
             AssertInValidState();
 
-            CurrentSession = _factory.OpenSession();
+            CurrentSession = _factory.OpenSession(_connectionProvider.GetConnection());
         }
 
         private void AssertInValidState()
