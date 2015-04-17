8be61857cbe6c31b7315450eb0fc92153af0dc76
Rob Scaduto <rob@thirdwave.it>
2015-04-17 12:46:14 -0400

    Refactored SqlExceptionConverter to make it configurable

--- src/Crux.Domain.Persistence/NHibernate/Config/SqlExceptionConverter.cs
+++ src/Crux.Domain.Persistence/NHibernate/Config/SqlExceptionConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 using NHibernate;
 using NHibernate.Exceptions;
 
@@ -7,42 +9,74 @@ namespace Crux.Domain.Persistence.NHibernate.Config
 {
     public class SqlExceptionConverter : ISQLExceptionConverter
     {
+        private readonly IList<SqlExceptionMapping> _mappings;
+
+        public static SqlExceptionConverter Default()
+        {
+            var mappings = new List<SqlExceptionMapping> {
+                SqlExceptionMapping.MatchOnSqlErrorCode(
+                    new[] {2627, 2601, 547},
+                    c => new ConstraintViolationException(c.SqlException.Message, c.SqlException.InnerException, c.Sql, null)
+                ),
+                SqlExceptionMapping.MatchOnSqlErrorCode(
+                    new[] {208},
+                    c => new SQLGrammarException(c.SqlException.Message, c.SqlException.InnerException, c.Sql)
+                ),
+                SqlExceptionMapping.MatchOnSqlErrorCode(
+                    new[] {3960},
+                    c => new StaleObjectStateException(c.EntityName, c.EntityId)
+                )
+            };
+
+            return new SqlExceptionConverter(mappings);
+        }
+
+        public SqlExceptionConverter(IList<SqlExceptionMapping> mappings)
+        {
+            _mappings = mappings;
+        }
+
         public Exception Convert(AdoExceptionContextInfo exceptionContext)
         {
-            var sqlException = ADOExceptionHelper.ExtractDbException(exceptionContext.SqlException) as SqlException;
+            var mapping = _mappings.FirstOrDefault(m => m.Matcher.Invoke(exceptionContext));
 
-            if (sqlException != null) {
-                switch (sqlException.Number) {
-                    case 2627:
-                    case 2601:
-                    case 547:
-                        return new ConstraintViolationException(
-                            exceptionContext.SqlException.Message,
-                            sqlException.InnerException,
-                            exceptionContext.Sql,
-                            null
-                        );
+            return mapping != null 
+                ? mapping.Converter.Invoke(exceptionContext) 
+                : SQLStateConverter.HandledNonSpecificException(exceptionContext.SqlException,exceptionContext.Message,exceptionContext.Sql);
+        }
 
-                    case 208:
-                        return new SQLGrammarException(
-                            exceptionContext.SqlException.Message,
-                            sqlException.InnerException,
-                            exceptionContext.Sql
-                        );
+        public void AddExceptionMappingToTop(SqlExceptionMapping mapping)
+        {
+            _mappings.Insert(0, mapping);
+        }
 
-                    case 3960:
-                        return new StaleObjectStateException(
-                            exceptionContext.EntityName,
-                            exceptionContext.EntityId
-                        );
-                }
-            }
+        public void AddExceptionMappingToEnd(SqlExceptionMapping mapping)
+        {
+            _mappings.Add(mapping);
+        }
 
-            return SQLStateConverter.HandledNonSpecificException(
-                exceptionContext.SqlException,
-                exceptionContext.Message,
-                exceptionContext.Sql
-            );
+        public void ClearMappings()
+        {
+            _mappings.Clear();
+        }
+    }
+
+    public class SqlExceptionMapping
+    {
+        public Func<AdoExceptionContextInfo, bool> Matcher { get; private set; }
+        public Func<AdoExceptionContextInfo, Exception> Converter { get; private set; }
+
+        private SqlExceptionMapping() { }
+
+        public static SqlExceptionMapping MatchOnSqlErrorCode(IEnumerable<int> errorCodes, Func<AdoExceptionContextInfo, Exception> converter)
+        {
+            return new SqlExceptionMapping {
+                Matcher = c => {
+                    var sqlException = ADOExceptionHelper.ExtractDbException(c.SqlException) as SqlException;
+                    return sqlException != null && errorCodes.Contains(sqlException.ErrorCode);
+                },
+                Converter = converter
+            };
         }
     }
 }
