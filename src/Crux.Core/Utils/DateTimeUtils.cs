798ae91536897ac15fca91b853f30a65041b3818
Leo Hernandez <lhernandez@thirdwave.it>
2015-05-21 17:52:04 -0400

    Date utils

---
+++ src/Crux.Core/Utils/DateTimeUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Crux.Core.Utils
+{
+    public static class DateTimeUtils
+    {
+        public static DateTime GetFirstDateOfCurrentYear()
+        {
+            return new DateTime(DateTime.Now.Year, 1, 1);
+        }
+
+        public static DateTime GetSqlServerMaxSmallDateTime()
+        {
+            return new DateTime(2079, 06, 06, 23, 59, 00);
+        }
+    }
+}
