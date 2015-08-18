fd13fde68e3684a91558076efd38d99b2d8eeb3b
Leo Hernandez <lhernandez@thirdwave.it>
2015-08-18 11:01:32 -0400

    Hexavigesimal number system

---
+++ src/Crux.Core.Tests/Types/Base26Tester.cs
@@ -0,0 +1,34 @@
+﻿using Crux.Core.Types;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Crux.Core.Tests.Types
+{
+    [TestFixture]
+    public class Base26Tester
+    {
+        [TestCase(1, "A")]
+        [TestCase(3485, "EDA")]
+        [TestCase(3973, "EVU")]
+        [TestCase(3999, "EWU")]
+        public void should_convert_alphabet_numbers_to_decimal(int decimalNumber, string alphaNumber)
+        {
+            ((Base26) decimalNumber).Should().Be((Base26) alphaNumber);
+        }
+
+        [TestCase("CPU", 2465)]
+        [TestCase("CUB", 2576)]
+        [TestCase("DID", 2942)]
+        public void should_conert_decimal_to_alphabet_number(string alphaNumber, int decimalNumber)
+        {
+            ((Base26)alphaNumber).Should().Be((Base26)decimalNumber);
+        }
+
+        [TestCase("A", "A", "B")]
+        [TestCase("Z", "A", "AA")]
+        public void should_be_able_to_do_some_arithmetic(string a, string b, string result)
+        {
+            ((Base26) a + (Base26) b).Should().Be((Base26)result);
+        }
+    }
+}
---
+++ src/Crux.Core/Types/Base26.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Crux.Core.Types
+{
+    //Hexavigesimal number system A = 1 ... Z = 26, AA = 27 ...
+    public class Base26
+    {
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public string AlphaNumber { get; }
+        public int DecimalNumber { get; }
+
+        public Base26(int decimalNumber)
+        {
+            DecimalNumber = decimalNumber;
+            AlphaNumber = ToAlphaNumber();
+        }
+
+        public Base26(string alphaNumber)
+        {
+            AlphaNumber = alphaNumber.ToUpper();
+            DecimalNumber = ToDecimal();
+        }
+
+        private int ToDecimal()
+        {
+            return AlphaNumber
+                .Reverse()
+                .Select((c, i) => (int)(ToDecimal(c) * Math.Pow(26, i)))
+                .Sum();
+        }
+
+        private string ToAlphaNumber()
+        {
+            var remainders = new List<int>();
+            var remainder = 0;
+
+            for (var counter = 1; remainder != DecimalNumber; counter++)
+            {
+                remainder = DecimalNumber % (int)(Math.Pow(26, counter));
+                remainders.Add(remainder);
+            }
+
+            var characters = remainders
+                .Select((r, i) => ToChar((int)Math.Floor(r / (Math.Pow(26, i)))))
+                .Reverse();
+
+            return string.Join(string.Empty, characters);
+        }
+
+        private static int ToDecimal(char c)
+        {
+            return Alphabet.IndexOf(c) + 1;
+        }
+
+        private static char ToChar(int decimalNumber)
+        {
+            return Alphabet[decimalNumber - 1];
+        }
+
+        #region Overrides and Overloads
+        public static explicit operator string(Base26 base26)
+        {
+            return base26.AlphaNumber;
+        }
+
+        public static explicit operator int (Base26 base26)
+        {
+            return base26.DecimalNumber;
+        }
+
+        public static explicit operator Base26(int decimalNumber)
+        {
+            return new Base26(decimalNumber);
+        }
+
+        public static explicit operator Base26(string alphaNumber)
+        {
+            return new Base26(alphaNumber);
+        }
+
+        public static Base26 operator + (Base26 a, Base26 b)
+        {
+            return new Base26(a.DecimalNumber + b.DecimalNumber);
+        }
+
+        public static Base26 operator - (Base26 a, Base26 b)
+        {
+            return new Base26(a.DecimalNumber - b.DecimalNumber);
+        }
+
+        public static Base26 operator * (Base26 a, Base26 b)
+        {
+            return new Base26(a.DecimalNumber * b.DecimalNumber);
+        }
+
+        public static Base26 operator / (Base26 a, Base26 b)
+        {
+            return new Base26(a.DecimalNumber / b.DecimalNumber);
+        }
+
+        protected bool Equals(Base26 other)
+        {
+            return string.Equals(AlphaNumber, other.AlphaNumber) && DecimalNumber == other.DecimalNumber;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((Base26)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((AlphaNumber != null ? AlphaNumber.GetHashCode() : 0) * 397) ^ DecimalNumber;
+            }
+        }
+        #endregion
+    }
+}
